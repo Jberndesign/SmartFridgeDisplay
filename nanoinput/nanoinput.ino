81ccfa7a60341fda1a0c911a8814c5f2df4c0ab8
Jason Bernotsky <jberndesign@gmail.com>
2013-04-19 13:49:10 -0400

    4/19/2013

---
+++ nanoinput/nanoinput.ino
@@ -0,0 +1,9 @@
+int Input = 1;
+ 
+void setup() {
+ pinMode(Input, INPUT);
+ }
+ 
+void loop() {
+ 
+}
---
+++ serialtransmit/serialtransmit.ino
@@ -0,0 +1,9 @@
+int Output = 0;
+
+void setup() {
+  pinMode(Output, OUTPUT);
+}
+
+void loop() {
+int Output= 100;
+}
